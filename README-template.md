2527df22df2f703b72970f4795df637cc1eea079
Ed <xzhang@intuition.com>
2023-07-28 16:38:52 +0100

    added delete/edit/upvote - draft reply function

--- README-template.md
+++ README-template.md
@@ -65,23 +65,11 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 
 ### What I learned
 
-Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+- how to make a modal and a overlay https://www.cssscript.com/minimal-overlay-modal-pure-css/
+- DOM nextElementSilbing/ nextSibling/ childNodes/ children differnece 
+- disable page scroll https://stackoverflow.com/questions/28411499/disable-scrolling-on-body
+- contenteditable attribute: https://stackoverflow.com/questions/70328976/create-a-edit-button-while-manipulating-dom-in-javascript
 
-To see how you can add code snippets, see below:
-
-```html
-<h1>Some HTML code I'm proud of</h1>
-```
-```css
-.proud-of-this-css {
-  color: papayawhip;
-}
-```
-```js
-const proudOfThisFunc = () => {
-  console.log('🎉')
-}
-```
 
 If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
 
--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('#container');
-const commentsDiv = document.querySelector('#comment');
+const commentsDiv = document.querySelector('#comments');
 const currentUserInputArea = document.querySelector('#current-user-comment');
 
 
@@ -8,7 +8,7 @@ function appendData(data) {
   currentUserInputArea.innerHTML = `
     <form action="">
       <textarea type="text" name="text" class="current-user-comment-text" placeholder="Add a comment..."></textarea>
-      <div class="user-send">
+      <div class="user-send-btn-wrapper">
         <img src="${data.currentUser.image.png}" alt="avatar">
         <button type="button" class="submit-btn">SEND</button>
       </div>
@@ -17,34 +17,97 @@ function appendData(data) {
   //populate the comment section
   for (let i = 0; i < data.comments.length; i++) {
     let eachComment = document.createElement('div');
-    eachComment.innerHTML = `
-        <div class="individualComment">
-          <div class="heading">
-            <img src="${data.comments[i].user.image.png}" alt="avatar">
-            <span class="name">${data.comments[i].user.username}</span>
-            <span class="time">${data.comments[i].createdAt}</span>
+    if (data.currentUser.username == data.comments[i].user.username) {
+      eachComment.innerHTML = `
+      <div class="individualComment">
+        <div class="heading">
+          <img src="${data.comments[i].user.image.png}" alt="avatar">
+          <span><span class="name">${data.comments[i].user.username}</span><span class="user-tag">You</span><span>
+          <span class="time">${data.comments[i].createdAt}</span>
+          <span class="delete" >Delete</span>
+          <span class="edit" >Edit</span>
+        </div>
+        <div class="content">
+          <p>${data.comments[i].content}</p>
+        </div>
+        <div class="comment-footer">
+          <div class="scoreButton">
+            <img src="images/icon-plus.svg" alt="upvote" class="upvote">
+            <span class="score"> ${data.comments[i].score} </span>
+            <img src="images/icon-minus.svg" alt="downvote">
           </div>
-          <div class="content">
-            <p>${data.comments[i].content}</p>
+          <div class="reply-btn-wrapper">
+            <img src="images/icon-reply.svg" alt="reply" class="downvote">
+            <button type="button" class="reply-btn">Reply</button>
           </div>
-          <div class="comment-footer">
-            <div class="scoreButton">
-              <img src="images/icon-plus.svg" alt="upvote">
-              <span class="score">&nbsp; ${data.comments[i].score} &nbsp;</span>
-              <img src="images/icon-minus.svg" alt="downvote">
-            </div>
-            <div class="reply-btn-wrapper">
-              <img src="images/icon-reply.svg" alt="reply">
-              <button type="button" class="reply-btn">Reply</button>
-            </div>
+        </div>
+    </div>
+      `;
+    } else {
+      eachComment.innerHTML = `
+      <div class="individualComment">
+        <div class="heading">
+          <img src="${data.comments[i].user.image.png}" alt="avatar">
+          <span class="name">${data.comments[i].user.username}</span>
+          <span class="time">${data.comments[i].createdAt}</span>
+        </div>
+        <div class="content">
+          <p>${data.comments[i].content}</p>
+        </div>
+        <div class="comment-footer">
+          <div class="scoreButton">
+            <img src="images/icon-plus.svg" alt="upvote" class="upvote">
+            <span class="score">${data.comments[i].score}</span>
+            <img src="images/icon-minus.svg" alt="downvote" class="downvote">
           </div>
-      </div>
-        `;
-    commentsDiv.append(eachComment);
+          <div class="reply-btn-wrapper">
+            <img src="images/icon-reply.svg" alt="reply">
+            <button type="button" class="reply-btn">Reply</button>
+          </div>
+        </div>
+    </div>
+      `;
+    }
 
+    commentsDiv.append(eachComment);
     for (let j = 0; j < data.comments[i].replies.length; j++) {
       let replies = document.createElement('div');
-      replies.innerHTML = `
+      console.log(data.currentUser.username);
+      console.log(data.comments[i].replies[j].user.username);
+      if (data.currentUser.username == data.comments[i].replies[j].user.username) {
+
+        replies.innerHTML = `
+        <div class="replies-wrapper">
+         <div class="replies">
+          <div class="heading">
+           <img src="${data.comments[i].replies[j].user.image.png}" alt="avatar">
+           <span><span class="name">${data.comments[i].user.username}</span><span class="user-tag">You</span></span>
+           <span class="time">${data.comments[i].replies[j].createdAt}</span>
+           <span class="delete" >Delete</span>
+           <span class="edit" >Edit</span>
+          </div>
+          <div class="content">
+          <p class="tag">&#64;${data.comments[i].replies[j].replyingTo}</p> 
+           <p>
+           ${data.comments[i].replies[j].content}
+           </p>
+          </div>
+          <div class="comment-footer">
+           <div class="scoreButton">
+             <img src="images/icon-plus.svg" alt="upvote" class="upvote">
+             <span class="score"> ${data.comments[i].replies[j].score}</span>
+             <img src="images/icon-minus.svg" alt="downvote" class="downvote">
+           </div>
+           <div class="reply-btn-wrapper">
+             <img src="images/icon-reply.svg" alt="reply">
+            <button type="button" class="reply-btn">Reply</button>
+           </div>
+          </div>
+         </div>
+        </div>
+         `;
+      } else {
+        replies.innerHTML = `
        <div class="replies-wrapper">
         <div class="replies">
         <div class="heading">
@@ -53,16 +116,16 @@ function appendData(data) {
           <span class="time">${data.comments[i].replies[j].createdAt}</span>
         </div>
         <div class="content">
+        <p class="tag">&#64;${data.comments[i].replies[j].replyingTo}</p>
           <p>
-          <span class="tag">&#64;${data.comments[i].replies[j].replyingTo}</span>
           ${data.comments[i].replies[j].content}
           </p>
         </div>
         <div class="comment-footer">
           <div class="scoreButton">
-            <img src="images/icon-plus.svg" alt="upvote">
-            <span class="score">&nbsp; ${data.comments[i].replies[j].score} &nbsp;</span>
-            <img src="images/icon-minus.svg" alt="downvote">
+            <img src="images/icon-plus.svg" alt="upvote" class="upvote">
+            <span class="score"> ${data.comments[i].replies[j].score}</span>
+            <img src="images/icon-minus.svg" alt="downvote" class="downvote">
           </div>
           <div class="reply-btn-wrapper">
             <img src="images/icon-reply.svg" alt="reply">
@@ -72,15 +135,15 @@ function appendData(data) {
         </div>
        </div>
         `;
+      }
+
       eachComment.append(replies);
     }
   }
 }
-
 // Function to add a new comment
 function addComment(data) {
   const sendBtns = document.querySelectorAll('.submit-btn');
-
   sendBtns.forEach((submitBtn) => {
     submitBtn.addEventListener('click', () => {
       console.log('click');
@@ -90,36 +153,152 @@ function addComment(data) {
       const currentUserNewComment = document.createElement('div');
       currentUserNewComment.innerHTML = `
       <div class="individualComment">
-      <div class="heading">
+       <div class="heading">
             <img src="${data.currentUser.image.png}" alt="avatar">
-            <span class="name">${data.currentUser.username}</span>
+            <span><span class="name">${data.currentUser.username}</span><span class="user-tag">You</span></span>
             <span class="time">Just Now</span>
-          </div>
-          <div class="content">
+            <span class="delete" >Delete</span>
+            <span class="edit">Edit</span>
+       </div>
+       <div class="content">
             <p>${commentInput}</p>
-          </div>
-          <div class="comment-footer" >
-            <div class="scoreButton">
+       </div>
+       <div class="comment-footer" >
+          <div class="scoreButton">
               <img src="images/icon-plus.svg" alt="upvote">
               <span class="score">&nbsp; 0 &nbsp;</span>
               <img src="images/icon-minus.svg" alt="downvote">
-            </div>
-            <div class="reply-btn-wrapper">
+          </div>
+          <div class="reply-btn-wrapper">
               <img src="images/icon-reply.svg" alt="reply">
               <button type="button" class="reply-btn">Reply</button>
-            </div>
           </div>
-          </div>
+       </div>
+      </div>
       `;
       commentsDiv.append(currentUserNewComment);
       document.querySelectorAll('.current-user-comment-text')[0].value = '';
+      deleteComment(data);
+    })
+  })
+
+}
+// Function to add a reply to a comment
+function addReply(data) {
+  const replyBtns = document.querySelectorAll('.reply-btn'); 
+  replyBtns.forEach((e)=>{
+    e.addEventListener('click', (e)=>{
+      const replyDiv = document.createElement('div'); 
+      let repliedToDiv = e.target.parentNode.parentNode.parentNode; 
+
+      console.log(data.currentUser.image.png)
+      if(repliedToDiv.classList.contains('replies')) {
+        replyDiv.innerHTML=`
+        <div class="replies">
+        <form>
+         <textarea type="text" name="text" class="current-user-comment-text" placeholder="Add a comment..."></textarea>
+         <div class="user-send-btn-wrapper">
+          <img src="${data.currentUser.image.png}"alt="avatar">
+          <button type="button" class="submit-btn">SEND</button>
+         </div>
+       </form>
+       </div>
+        `;
+      } else {
+        replyDiv.innerHTML=`
+        <div class="individualComment">
+        <form>
+         <textarea type="text" name="text" class="current-user-comment-text" placeholder="Add a comment..."></textarea>
+         <div class="user-send-btn-wrapper">
+          <img src="${data.currentUser.image.png}" alt="avatar">
+          <button type="button" class="submit-btn">SEND</button>
+         </div>
+       </form>
+       </div>
+        `;
+      }
+      repliedToDiv.insertAdjacentHTML("afterend", replyDiv.innerHTML);
+      
     })
   })
 }
 
-// Function to add a reply to a comment
-function addReply() {
+function deleteComment() {
+  const deleteBtns = document.querySelectorAll('.delete');
+  const confirmBtn = document.getElementById('confirm');
+  const cancelBtn = document.getElementById('cancel');
+  const overlay = document.getElementById('overlay');
+  const modal = document.getElementById('modal');
 
+  deleteBtns.forEach((e) => {
+    e.addEventListener('click', () => {
+      console.log('delete clicked')
+      window.scrollTo(0, 0);
+      overlay.classList.remove('hidden');
+      modal.classList.remove('hidden');
+    })
+
+    confirmBtn.addEventListener('click', () => {
+      e.parentNode.parentNode.parentNode.remove();
+      modal.classList.add('hidden');
+      overlay.classList.add('hidden')
+    })
+
+    cancelBtn.addEventListener('click', () => {
+      modal.classList.add('hidden');
+      overlay.classList.add('hidden')
+    })
+
+  })
+}
+
+function editComment() {
+  const editBtns = document.querySelectorAll('.edit'); 
+
+  editBtns.forEach((btn)=>[
+    btn.addEventListener('click', (e)=>{
+      console.log(e.target.parentNode.nextElementSibling.childNodes[3]); 
+      e.target.parentNode.nextElementSibling.childNodes[3].setAttribute('contenteditable', 'true'); 
+      e.target.parentNode.nextElementSibling.childNodes[3].focus(); 
+
+      const updateBtn = document.createElement('button'); 
+      updateBtn.classList.add('update-btn');
+      updateBtn.innerText='UPDATE'; 
+      console.log(updateBtn)
+  
+      e.target.parentNode.nextElementSibling.append(updateBtn); 
+
+      updateBtn.addEventListener('click', ()=>{
+        e.target.parentNode.nextElementSibling.childNodes[3].setAttribute('contenteditable', 'false'); 
+        updateBtn.classList.add('hidden')
+      })
+    })
+  ])
+}
+
+function upvote() {
+  const upvoteBtns = document.querySelectorAll('.upvote'); 
+  const downvoteBtns = document.querySelectorAll('.downvote'); 
+
+  upvoteBtns.forEach((e)=>{
+    e.addEventListener('click', (e)=>{
+      let scoreCurrent = e.target.nextElementSibling.innerText; 
+      scoreCurrent++; 
+      e.target.nextElementSibling.innerHTML = `${scoreCurrent}`; 
+  })
+  })
+}
+
+function downvote() {
+  const downvoteBtns = document.querySelectorAll('.downvote'); 
+
+  downvoteBtns.forEach((e)=>{
+    e.addEventListener('click', (e)=>{
+      let scoreCurrent = e.target.previousElementSibling.innerText; 
+      scoreCurrent--; 
+      e.target.previousElementSibling.innerText = scoreCurrent; 
+  })
+  })
 }
 
 fetch('../data.json')
@@ -128,8 +307,12 @@ fetch('../data.json')
   }) //response.json() returns a promise - so we can now use another .then() to get the value,
   .then((data) => {
     appendData(data);
-    addComment(data); 
-    //addReply(data);
+    addComment(data);
+    addReply(data);
+    deleteComment();
+    editComment(); 
+    upvote(); 
+    downvote(); 
   })
   .catch((err) => {
     console.log(err);
